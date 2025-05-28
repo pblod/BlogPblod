78ebe725fce8d6301f74cee26afb9b940674ebff
Pblod <Ben12tend@gmail.com>
2025-05-28 17:01:45 +0700

    KYS vercel

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,20 +1,16 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { authConfig } from "@/lib/kinde";
-import { NextResponse } from "next/server";
-import { type NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, context: { params: { kindeAuth: string } }) {
-  if (!process.env.KINDE_CLIENT_ID || !process.env.KINDE_CLIENT_SECRET || !process.env.KINDE_ISSUER_URL) {
-    return new NextResponse('Kinde configuration missing', { status: 500 });
-  }
-  
-  const handler = handleAuth({
+export function GET(
+  request: NextRequest,
+  { params }: { params: { kindeAuth: string } }
+) {
+  return handleAuth({
     clientId: authConfig.clientId,
     clientSecret: authConfig.clientSecret,
     issuer: authConfig.issuerUrl,
     redirectUri: authConfig.redirectUrl,
     logoutUri: authConfig.logoutUrl,
-  });
-  
-  return handler(request, context);
+  })(request, { params });
 }
