edb1a2bd92b65f347f960960f269cbda2645d054
Pblod <Ben12tend@gmail.com>
2025-05-28 16:47:18 +0700

    fix(2)2

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,13 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  env: {
+    KINDE_ISSUER_URL: process.env.KINDE_ISSUER_URL,
+    KINDE_CLIENT_ID: process.env.KINDE_CLIENT_ID,
+    KINDE_CLIENT_SECRET: process.env.KINDE_CLIENT_SECRET,
+    KINDE_REDIRECT_URL: process.env.KINDE_REDIRECT_URL,
+    KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
+  },
 };
 
 export default nextConfig;
