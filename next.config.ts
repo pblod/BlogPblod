242010025c62904a37636f1a8c0c7a2889ca2f1c
Pblod <Ben12tend@gmail.com>
2025-05-28 17:03:59 +0700

    Pleaseeeegit add .

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -17,6 +16,7 @@ const nextConfig: NextConfig = {
     KINDE_CLIENT_SECRET: process.env.KINDE_CLIENT_SECRET,
     KINDE_REDIRECT_URL: process.env.KINDE_REDIRECT_URL,
     KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
+    KINDE_SITE_URL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000",
   },
 };
 
--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,16 +1,3 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { authConfig } from "@/lib/kinde";
-import { NextRequest } from "next/server";
 
-export function GET(
-  request: NextRequest,
-  { params }: { params: { kindeAuth: string } }
-) {
-  return handleAuth({
-    clientId: authConfig.clientId,
-    clientSecret: authConfig.clientSecret,
-    issuer: authConfig.issuerUrl,
-    redirectUri: authConfig.redirectUrl,
-    logoutUri: authConfig.logoutUrl,
-  })(request, { params });
-}
+export const GET = handleAuth();
